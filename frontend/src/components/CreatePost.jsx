bf9d6aa937bbe22ff12ea477273adfe9c1d041ef
Tikaram <tikarammahato377@gmail.com>
2024-11-21 04:19:21 +0530

    ready for deploy

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -7,6 +7,7 @@ import { readFileAsDataURL } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { toast } from 'sonner';
 import axios from 'axios';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { setPosts } from '@/redux/postSlice';
 
